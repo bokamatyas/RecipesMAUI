d249dd4d3148dfb4d470e81b0dfaabcc45d8ef09
Mátyás Bóka <72026133+bokamatyas@users.noreply.github.com>
2025-01-03 16:07:40 +0100

    Creating WIP style for MainPage

--- Recipes/ViewModels/MainViewModel.cs
+++ Recipes/ViewModels/MainViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Recipes.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +12,29 @@ namespace Recipes.ViewModels
 {
     public partial class MainViewModel: ObservableObject
     {
+        [ObservableProperty]
+        public List<RecipeDataModel>? recipesData;
+
+        [RelayCommand]
+        private void Appearing()
+        {
+            try
+            {
+                RecipesData = Task.Run(() => RecipeDataBase.GetAllItemsAsync()).Result;
+            }
+            catch (Exception ex)
+            {
+#if DEBUG
+                Debug.WriteLine(ex);
+#endif
+                return;
+            }
+        }
+
+        [RelayCommand]
+        private void ViewRecipe() 
+        {
+
+        }
     }
 }
